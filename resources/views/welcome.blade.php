043ceaaef3c65fd5811981e5f6d7cbf23650c5e5
unknown <bylasuhaimi@gmail.com>
2020-07-20 11:29:16 +0800

    change blog name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    my blog
                 </div>
 
                 <div class="links">
